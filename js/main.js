8ffb135ec764d4d5a1b35eef00b1f9df0e2e5f83
juan-zv <juanzuritavasquez@gmail.com>
2024-02-09 04:12:45 -0700

    added biggger changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 
-document.querySelector('.burger').addEventListener('click', function() {
+document.querySelector('.burger').addEventListener('click', function () {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
-  });
+});
+
+// Select Juan Zurita and change its font-style every 1.25 secs
+var myName = document.getElementById("my-name");
+var fontStyles = ["rubik-doodle", "bebas-neue", "lobster", "caveat"];
+var i = 0;
+
+setInterval(function () {
+    myName.classList.remove(fontStyles[i % fontStyles.length]);
+    i++;
+    myName.classList.add(fontStyles[i % fontStyles.length]);
+}, 1250);
